a720b4ddeb450c40f3b05d7eb84019abaeda02ac
许恒亮 <hengliang.xu@patpat.com>
2022-08-16 15:19:00 +0800

    两数之和 算法

--- src/main/java/com/liufang/leecode/day20220816/TwoNum.java
+++ src/main/java/com/liufang/leecode/day20220816/TwoNum.java
@@ -1,5 +1,9 @@
 package com.liufang.leecode.day20220816;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author hengliang.xu@patpat.com
  * @decription
@@ -7,4 +11,24 @@ package com.liufang.leecode.day20220816;
  */
 public class TwoNum {
 
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int other = target - nums[i];
+            if (map.containsKey(other)) {
+                return new int[]{map.get(other),i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
 }
